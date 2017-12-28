c788395bfbcb9d62989f523d46b81b5422c87039
Tyquan Reddick <Tjreddick@gmail.com>
2017-12-28 01:35:21 -0500

    updated ux

--- public/js/randomword.js
+++ public/js/randomword.js
@@ -2,7 +2,37 @@ function getRandomQoute() {
 	var qoutes = ["Learn HTML5", "Learn Javascript", "Build A Website", "Learn PHP", "Learn Nodejs", "Learn Mongodb", "Learn Agularjs", "Learn MYSQL", "Learn Python", "Build an App", "Virtual Training", "Skype Avaiable", "Ecommerce Development", "Custom Applications", "Learn ES6", "Learn Jquery", "Learn CSS3"];
 	var randomNumber = Math.floor(Math.random() * (qoutes.length));
 	var random = qoutes[randomNumber];
+
 	document.getElementById('randomWord').innerHTML = qoutes[randomNumber];
 }
 
-setInterval(function(){ getRandomQoute() }, 1000);
+function getRandomColor(){
+	var colors = [
+		"F03434",
+		"D91E18",
+		"F22613",
+		"CF000F",
+		"663399",
+		"9A12B3",
+		"013243",
+		"E4F1FE",
+		"1F3A93",
+		"26A65B",
+		"019875",
+		"1E824C",
+		"FABE58",
+		"F89406",
+		"F2784B",
+		"EB9532",
+		"F9690E"
+	];
+	var randomColor = Math.floor(Math.random() * (colors.length));
+	var random = colors[randomColor];
+
+	document.getElementById('randomWord').style.color = "#" + random;
+}
+
+setInterval(function(){ 
+	getRandomQoute();
+	getRandomColor();
+}, 1000);
