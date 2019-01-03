fe4780a0a00ea9338992eabeb4f5c0671e091f06
Tyquan <tyquanjreddick@gmail.com>
2019-01-03 13:33:19 -0500

    updated

---
+++ public/js/randomword.js
@@ -0,0 +1,60 @@
+function getRandomQoute() {
+	var qoutes = [
+		"Strategy & Consulting",
+		"Website Development",
+		"App Development",
+		"Digital Marketing",
+		"Social Media Marketing",
+		"Advertising",
+		"Content Strategy",
+		"Data Analytics",
+		"Brand Strategy",
+		"Business Consulting",
+		"Logo Design",
+		"Email Marketing",
+		"Marketing Strategy",
+		"Search Engine Optimization",
+		"Content Marketing",
+		"Learn HTML5",
+		"Learn CSS3", 
+		"Learn Javascript",
+		"Virtual Training", 
+		"Skype Avaiable", 
+		"Ecommerce Development"
+	];
+	var randomNumber = Math.floor(Math.random() * (qoutes.length));
+	var random = qoutes[randomNumber];
+
+	document.getElementById('randomWord').innerHTML = qoutes[randomNumber];
+}
+
+function getRandomColor(){
+	var colors = [
+		"F03434",
+		"D91E18",
+		"F22613",
+		"CF000F",
+		"663399",
+		"9A12B3",
+		"013243",
+		"E4F1FE",
+		"1F3A93",
+		"26A65B",
+		"019875",
+		"1E824C",
+		"FABE58",
+		"F89406",
+		"F2784B",
+		"EB9532",
+		"F9690E"
+	];
+	var randomColor = Math.floor(Math.random() * (colors.length));
+	var random = colors[randomColor];
+
+	document.getElementById('randomWord').style.color = "#" + random;
+}
+
+setInterval(function(){ 
+	getRandomQoute();
+	getRandomColor();
+}, 1500);
